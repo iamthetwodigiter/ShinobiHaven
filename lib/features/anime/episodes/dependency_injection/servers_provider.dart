3d3b55feb0da05ebf28e6fd7f7c139cc91be6866
Prabhat Jana <itsmeprabhatjana@gmail.com>
2025-10-26 12:02:06 +0530

    fix: correct import and class name for ServersViewmodel

--- lib/features/anime/episodes/dependency_injection/servers_provider.dart
+++ lib/features/anime/episodes/dependency_injection/servers_provider.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shinobihaven/features/anime/episodes/model/servers.dart';
 import 'package:shinobihaven/features/anime/episodes/repository/servers_repository.dart';
-import 'package:shinobihaven/features/anime/episodes/viewmodel/servers_viewmode.dart';
+import 'package:shinobihaven/features/anime/episodes/viewmodel/servers_viewmodel.dart';
 
 final serversRepositoryProvider = Provider((ref) => ServersRepository());
 
-final serversViewModelProvider = StateNotifierProvider.family<ServersViewmode, AsyncValue<ServersData>, String>((ref, animeSlug) {
+final serversViewModelProvider = StateNotifierProvider.family<ServersViewmodel, AsyncValue<ServersData>, String>((ref, animeSlug) {
   final serversRepository = ref.watch(serversRepositoryProvider);
-  return ServersViewmode(serversRepository);
+  return ServersViewmodel(serversRepository);
 });
--- lib/features/anime/episodes/viewmodel/servers_viewmodel.dart
+++ lib/features/anime/episodes/viewmodel/servers_viewmodel.dart
@@ -2,9 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shinobihaven/features/anime/episodes/model/servers.dart';
 import 'package:shinobihaven/features/anime/episodes/repository/servers_repository.dart';
 
-class ServersViewmode extends StateNotifier<AsyncValue<ServersData>> {
+class ServersViewmodel extends StateNotifier<AsyncValue<ServersData>> {
   final ServersRepository _repo;
-  ServersViewmode(this._repo) : super(AsyncValue.loading());
+  ServersViewmodel(this._repo) : super(AsyncValue.loading());
 
   Future<void> fetchServers(String episodeID) async {
     state = AsyncValue.loading();
